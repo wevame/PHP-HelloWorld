ddf4625f0f2aba6e256d4bdcca536609ad246b8a
wevame <wvanmele@gmail.com>
2019-05-02 21:47:53 +0200

    Update index.php
    small title change

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
  <head>
-  <title>PHP Test</title>
+  <title>PHP Test v2 by WVM</title>
  </head>
  <body>
  <?php echo '<p>Hello World-Happy Learning. This is wonderful! This is my first Jenkins PHP200 page using OpenShift Online</p>'; ?> 
