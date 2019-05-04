2d143240c5da3dc46a8fd7d197d707a3c3c25ab2
wevame <wvanmele@gmail.com>
2019-05-04 10:27:55 +0200

    Update index.php
    s;all change for demonstration purposes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
  <head>
-  <title>PHP Test v2 by WVM</title>
+  <title>Openshift devops training 03/05/2019</title>
  </head>
  <body>
  <?php echo '<p>Hello World-Happy Learning. This is wonderful! This is my first Jenkins PHP200 page using OpenShift Online</p>'; ?> 
